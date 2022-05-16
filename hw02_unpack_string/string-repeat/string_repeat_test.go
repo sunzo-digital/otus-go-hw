dc868df0ee17036158e255ae0b9d622f2f6e1120
sunzo <i.sunco@devspark.ru>
2022-05-16 22:40:03 +0600

    HW2 is completed

---
+++ hw02_unpack_string/string-repeat/string_repeat_test.go
@@ -0,0 +1,26 @@
+package string_repeat
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestUnpack(t *testing.T) {
+	tests := []struct {
+		strToRepeat StringToRepeat
+		expected    string
+	}{
+		{New("a", 1), "a"},
+		{New("b", 5), "bbbbb"},
+		{New("c", 0), ""},
+	}
+
+	for _, tc := range tests {
+		tcName := fmt.Sprintf("%s * %d", tc.strToRepeat.str, tc.strToRepeat.repeatCount)
+		t.Run(tcName, func(t *testing.T) {
+			require.Equal(t, tc.expected, tc.strToRepeat.ToString())
+		})
+	}
+}
--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -1,12 +1,73 @@
-package hw02unpackstring
+package main
 
 import (
 	"errors"
+	"strconv"
+	"strings"
+	"unicode"
+	"unicode/utf8"
+
+	string_repeat "github.com/sunzo-digital/otus-go-hw/hw02_unpack_string/string-repeat"
 )
 
 var ErrInvalidString = errors.New("invalid string")
 
-func Unpack(_ string) (string, error) {
-	// Place your code here.
-	return "", nil
+func Unpack(input string) (string, error) {
+	if input == "" {
+		return "", nil
+	}
+
+	firstRune := firstRuneOfString(input)
+
+	if unicode.IsDigit(firstRune) {
+		return "", ErrInvalidString
+	}
+
+	stringsToRepeat, err := stringsToRepeat(input)
+
+	if err != nil {
+		return "", err
+	}
+
+	strBuilder := strings.Builder{}
+
+	for _, strToRepeat := range stringsToRepeat {
+		strBuilder.WriteString(strToRepeat.ToString())
+	}
+
+	return strBuilder.String(), nil
+}
+
+func stringsToRepeat(input string) ([]string_repeat.StringToRepeat, error) {
+	stringsToRepeat := make([]string_repeat.StringToRepeat, utf8.RuneCountInString(input))
+
+	prev := firstRuneOfString(input)
+
+	for _, current := range input {
+		if unicode.IsLetter(current) {
+			strToRepeat := string_repeat.New(string(current), 1)
+			stringsToRepeat = append(stringsToRepeat, strToRepeat)
+			continue
+		}
+
+		// 2 цифры подряд
+		if unicode.IsDigit(prev) {
+			return nil, ErrInvalidString
+		}
+
+		prevStrToRepeat := &stringsToRepeat[len(stringsToRepeat)-1]
+		repeatCount, err := strconv.Atoi(string(current))
+
+		if err != nil {
+			return nil, err
+		}
+
+		prevStrToRepeat.SetRepeatCount(repeatCount)
+	}
+
+	return stringsToRepeat, nil
+}
+
+func firstRuneOfString(input string) rune {
+	return []rune(input)[0]
 }
