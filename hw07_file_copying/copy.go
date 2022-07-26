418c2ed45cb5483339e60b09c9af5107bc7002ac
sunzo <sunzo.digital@gmail.com>
2022-07-26 21:55:38 +0600

    hw7 is completed

--- hw07_file_copying/copy.go
+++ hw07_file_copying/copy.go
@@ -2,14 +2,60 @@ package main
 
 import (
 	"errors"
+	"io"
+	"os"
+
+	"github.com/cheggaaa/pb/v3"
 )
 
 var (
-	ErrUnsupportedFile       = errors.New("unsupported file")
 	ErrOffsetExceedsFileSize = errors.New("offset exceeds file size")
+	ErrUnknownFileSize       = errors.New("unknown size of the file")
 )
 
 func Copy(fromPath, toPath string, offset, limit int64) error {
-	// Place your code here.
-	return nil
+	srcFile, err := os.Open(fromPath)
+	if err != nil {
+		return err
+	}
+
+	srcFileInfo, err := srcFile.Stat()
+	if err != nil {
+		return err
+	}
+
+	srcFileSize := srcFileInfo.Size()
+
+	if srcFileSize == 0 {
+		return ErrUnknownFileSize
+	}
+
+	if offset > srcFileSize {
+		return ErrOffsetExceedsFileSize
+	}
+
+	dstFile, err := os.Create(toPath)
+	if err != nil {
+		return err
+	}
+
+	_, err = srcFile.Seek(offset, io.SeekStart)
+
+	if err != nil {
+		return err
+	}
+
+	if limit == 0 || limit > srcFileSize {
+		limit = srcFileSize
+	}
+
+	bar := pb.Full.Start64(limit - offset)
+
+	barReader := bar.NewProxyReader(srcFile)
+
+	_, err = io.CopyN(dstFile, barReader, limit)
+
+	bar.Finish()
+
+	return err
 }
